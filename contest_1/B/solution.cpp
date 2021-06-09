08cf52651c151f451e17144a53ac1b314e9eb6e5
Olya Const <elyaishere@mail.ru>
2021-06-10 01:34:47 +0300

    Create solution.cpp

---
+++ contest_1/B/solution.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    if (a + b <= c || a + c <= b || b + c <= a)
+        cout << "NO\n";
+    else 
+        cout << "YES\n";
+    return 0;
+}
